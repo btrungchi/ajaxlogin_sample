d7dd6bb71d42f203da9bd494bda5469775b06fff
Bui Trung Chi <btchi@Chi.HP.Probook>
2017-01-21 22:54:59 +0700

    update

---
+++ public/login.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+   $("#login").click(function(){  
+        email=$("#email").val();
+        password=$("#password").val();
+        if(email=='' || password==''){
+          $("#result").html("Please fill in both your email and password");
+        } else {
+            $.ajax({
+            type: "POST",
+            url: "/login",
+            data: "email="+email+"&password="+password,
+            success: function(jsonData){
+              console.log(jsonData);              
+              email = jsonData.email;
+              loginSuccess = jsonData.loginSuccess;
+
+              if (email && loginSuccess == "Yes") {
+                $("#result").html(email + " login successed");
+              } else {
+                $("#result").html("Wrong email/password");
+              }
+            }
+            });
+            return false;
+        }  
+    });
+});
---
+++ src/main/java/app/App.java
@@ -0,0 +1,48 @@
+package app;
+
+import java.util.Map;
+import java.util.HashMap;
+import static spark.Spark.*;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import java.nio.file.Paths;
+
+/**
+ * Main class
+ *
+ */
+public class App 
+{
+    public static final String STATIC_PATH = "/public";
+    public static final String ROUTE_LOGIN = "/login";
+
+    public static Route loginPostAction = (Request request, Response response) -> {
+        Map<String, String> loginResult = new HashMap<String, String>();
+        
+        // Get query data
+        String email = request.queryParams("email");
+        String password = request.queryParams("password");
+
+        loginResult.put("email", email);
+
+        // Check valid account
+        if (email.equals("test@abc.com") && password.equals("test")) {
+            loginResult.put("loginSuccess", "Yes");
+        } else {
+            loginResult.put("loginSuccess", "No");
+        }
+
+        return Utils.sendJsonContent(request, response, loginResult);
+    };
+    public static void main( String[] args )
+    {   
+        String currDirectory = Paths.get(".").toAbsolutePath().normalize().toString();
+
+        externalStaticFileLocation(currDirectory + App.STATIC_PATH);
+        setIpAddress("localhost");
+        setPort(8080);        
+
+        post(App.ROUTE_LOGIN, App.loginPostAction);
+    }    
+}
